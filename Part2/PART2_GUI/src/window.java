5882a7d80b83be8f6113f49aef73e468732d46d1
Zaynab <ec23257@qmul.ac.uk>
2025-04-16 15:12:43 +0100

    transfered winning horse announcement to GUI, created buttons and removed print statements in the Race class

--- Part2/PART2_GUI/src/window.java
+++ Part2/PART2_GUI/src/window.java
@@ -1,47 +1,73 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+
 
 public class window {
     public static void main(String[] args){
         //creating main window
 
         //creating JFrame called frame
-            JFrame frame = new JFrame("Horse Race Sim");
+        JFrame frame = new JFrame("Horse Race Sim");
 
-            //setting the frame size
-            frame.setSize(800,600);
+        //setting the frame size
+        frame.setSize(800,600);
 
-            //when x is pressed, frame will close
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            //able to see the frame
-            frame.setVisible(true);
-
-
-
+        //when x is pressed, frame will close
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //using borderLayout to divide the window into the different areas
         frame.setLayout(new BorderLayout());
 
-        //adding raceTrack panel to the main frame
-        raceTrack track = new raceTrack();
+        //able to see the frame
+        frame.setVisible(true);
+
+
+        //logic setup
+        Horse pinky = new Horse('◇', "PINKY", 0.8);
+        Horse william = new Horse('◆', "WILLIAM", 0.6);
+        Horse pipi = new Horse('♞', "PIPI", 0.5);
+
+        Race race = new Race(30);
+        race.addHorse(pinky, 1);
+        race.addHorse(william, 2);
+        race.addHorse(pipi, 3);
+        race.startRace();
+
 
 
         //creating instance of raceTrack panel
-        raceTrack trackPanel = new raceTrack();
+        raceTrack trackPanel = new raceTrack(pinky,william,pipi);
+        frame.add(trackPanel, BorderLayout.CENTER);
 
         //create a start button
         JButton startButton = new JButton("Start");
+        frame.add(startButton, BorderLayout.SOUTH);
 
 
+        //timer setup
+        Timer timer = new Timer(100,new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                race.updateRaceState();
+                trackPanel.repaint();
+
+                if(race.isRaceFinished()){
+                    ((Timer) e.getSource()).stop();
+
+                    JOptionPane.showMessageDialog(frame, race.getWinnerNames());
+                }//END if
+            }//END actionPerformed
+        });
 
 
-
-        //adding things to the frame
-        frame.add(track);
-
-            // Add it to the center of the window
-            frame.add(trackPanel, BorderLayout.CENTER);
+        //Button action
+        startButton.addActionListener(e -> {
+            race.startRace();
+            timer.start();
+        });
     }//END main
 
 
